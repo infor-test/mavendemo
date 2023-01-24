80bcc0affe2071551dc14602516c7c946255fa5a
kishore <kishore.t533@gmail.com>
2023-01-24 07:14:04 +0530

    added maven project

---
+++ src/main/java/wipro/andhrabank/internet/App.java
@@ -0,0 +1,13 @@
+package wipro.andhrabank.internet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
